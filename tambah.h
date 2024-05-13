d2e9fa46af73785a6141b6844264d5e74061d819
firahmaulida <163720090+firahmaulida@users.noreply.github.com>
2024-05-13 23:03:14 +0700

    Create tambah.h

---
+++ tambah.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NAMA_FILE "tambah.txt"
+
+struct Barang {
+    int nomor;
+    char nama[50];
+    float harga;
+    int stok;
+};
+
+void tambahBarang(struct Barang *daftarBarang, int numBarang) {
+    FILE *file = fopen(NAMA_FILE, "a"); 
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    for (int i = 0; i < numBarang; i++) {
+        fprintf(file, "%d %s %.2f %d\n", daftarBarang[i].nomor, daftarBarang[i].nama, daftarBarang[i].harga, daftarBarang[i].stok);
+    }
+
+    fclose(file);
+}
