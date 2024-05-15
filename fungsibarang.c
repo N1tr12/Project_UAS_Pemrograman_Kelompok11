b8630f5fa88d46aed817ab27f7b3f16bd539ee25
firahmaulida <163720090+firahmaulida@users.noreply.github.com>
2024-05-15 19:34:45 +0700

    Create fungsibarang.c

---
+++ fungsibarang.c
@@ -0,0 +1,31 @@
+#include "fungsibarang.h"
+
+void tambahBarang(struct Barang *daftarBarang, int numBarang) {
+    FILE *file = fopen(NAMA_FILE, "a"); 
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    for (int i = 0; i < numBarang; i++) {
+        fprintf(file, "%d %s %.2f %d\n", daftarBarang[i].nomor, daftarBarang[i].nama, daftarBarang[i].harga, daftarBarang[i].stok);
+    }
+    
+    fclose(file);
+}
+
+void lihatDaftarBarang() {
+    FILE *file = fopen(NAMA_FILE, "r");
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    struct Barang barang;
+    printf("Daftar Barang:\n");
+    while (fscanf(file, "%d %s %f %d", &barang.nomor, barang.nama, &barang.harga, &barang.stok) != EOF) {
+        printf("%d %s %.2f %d\n", barang.nomor, barang.nama, barang.harga, barang.stok);
+    }
+
+    fclose(file);
+}
