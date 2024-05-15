bf52139336a9a91e29967bbd052c796bf043e3e9
firahmaulida <163720090+firahmaulida@users.noreply.github.com>
2024-05-15 19:30:06 +0700

    Update fungsibarang.h

--- fungsibarang.h
+++ fungsibarang.h
@@ -1,3 +1,6 @@
+#ifndef FUNGSIBARANG_H
+#define FUNGSIBARANG_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,32 +13,7 @@ struct Barang {
     int stok;
 };
 
-void tambahBarang(struct Barang *daftarBarang, int numBarang) {
-    FILE *file = fopen(NAMA_FILE, "a"); 
-    if (file == NULL) {
-        printf("Gagal membuka file.\n");
-        return;
-    }
+void tambahBarang(struct Barang *daftarBarang, int numBarang);
+void lihatDaftarBarang();
 
-    for (int i = 0; i < numBarang; i++) {
-        fprintf(file, "%d %s %.2f %d\n", daftarBarang[i].nomor, daftarBarang[i].nama, daftarBarang[i].harga, daftarBarang[i].stok);
-    }
-    
-    fclose(file);
-}
-
-void lihatDaftarBarang() {
-    FILE *file = fopen(NAMA_FILE, "r");
-    if (file == NULL) {
-        printf("Gagal membuka file.\n");
-        return;
-    }
-
-    struct Barang barang;
-    printf("Daftar Barang:\n");
-    while (fscanf(file, "%d %s %f %d", &barang.nomor, barang.nama, &barang.harga, &barang.stok) != EOF) {
-        printf("%d %s %.2f %d\n", barang.nomor, barang.nama, barang.harga, barang.stok);
-    }
-
-    fclose(file);
-}
+#endif
